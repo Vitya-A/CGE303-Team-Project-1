8d8eaa9468fe915f9245c1a3585331b45ab947dc
stefch07 <schar9360@365.elmhurst.edu>
2024-03-03 21:42:05 -0600

    timer

--- MedRun/Assets/Script/TimerText.cs
+++ MedRun/Assets/Script/TimerText.cs
@@ -13,9 +13,19 @@ public class TimerText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            // GameOver):
+            timerText.color = Color.red;
+
+        }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}: {1:00}", minutes, seconds);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
