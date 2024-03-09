32eb897cfca3cd2aa78745e68daba4c787aa7a05
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-03-09 15:31:47 -0600

    SpikeTriggerZone script.

--- MedRun/Assets/Script/SpikeTriggerZone.cs
+++ MedRun/Assets/Script/SpikeTriggerZone.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SpikeTriggerZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text output;
+    public string textToDisplay;
+    public static bool gameOver;
+
+    private void Start()
     {
-        
+        gameOver = false;
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gameOver = true;
+            output.text = textToDisplay;
+        }
+    }
+
     void Update()
     {
-        
+        if (gameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            /*CharacterController cc = GetComponent<CharacterController>();
+            cc.enabled = false;*/
+        }
     }
 }
